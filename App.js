9baae4d73324e42036e77ac2882373b517c86e82
chamathkadinusahani <chamathkadinusahani1@gmail.com>
2024-09-12 02:16:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/home/HomePage/HomePage';
-import WelcomePage from './components/welcomelogin/WelcomePage';
-import LoginPage from './components/studentlogin/LoginPage/LoginPage';
-import TeacherLogin from './components/TeacherLogin/TeacherLogin';
-import AdminLogin from './components/AdminLogin/AdminLogin';
-import AdminRegister from './components/AdminRegister/AdminRegister';
-import Login02 from './components/studentregister/Login02/Login02';
-import TeacherRegister from './components/TeacherRegister/TeacherRegister';
+import HomePage from './home/HomePage/HomePage';
+import WelcomePage from './welcomelogin/WelcomePage';
+import LoginPage from './studentlogin/LoginPage/LoginPage';
+import TeacherLogin from './TeacherLogin/TeacherLogin';
+import AdminLogin from './AdminLogin/AdminLogin';
+import AdminRegister from './AdminRegister/AdminRegister';
+import Login02 from './studentregister/Login02/Login02';
+import TeacherRegister from './TeacherRegister/TeacherRegister';
 import ABCSchool from './aws-components/ABCSchool';
 import { ThemeProvider } from '@aws-amplify/ui-react';
 import studioTheme from './aws-components/studioTheme';
-import Footer from './components/Home1/Footer';
-import Home from './components/home/HomePage/HomePage';
-import CourseDetails from './components/Coursehome/CourseDetails';
-import StudentBio from './components/studentbio/StudentBio';
-import Header from './components/home/Header/Header';
-import Payments from './components/payment/MyComponent';
+import Footer from './Home1/Footer';
+import Home from './home/HomePage/HomePage';
+import CourseDetails from './Coursehome/CourseDetails';
+import StudentBio from './studentbio/StudentBio';
+import Header from './home/Header/Header';
+import Payments from './payment/MyComponent';
 
 
 function App() {
